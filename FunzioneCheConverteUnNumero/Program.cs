bdc047a0e153e8e101dbade2acc2374782192985
_ Trentini _ <pietrotrentini5@gmail.com>
2020-11-09 09:09:54 +0100

    creazione di un ramo di sviluppo con inserimento di funzione padlef

---
+++ FunzioneCheConverteUnNumero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FunzioneCheConverteUnNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String number;
+            int fromBase;
+            int toBase;
+            Console.Write("inserisci il numero da convertire: ");
+            number = Console.ReadLine();
+            Console.Write("inserisci la base da cui convertire : ");
+            fromBase = int.Parse(Console.ReadLine());
+            Console.Write("inserisci la base a cui convertire: ");
+            toBase = int.Parse(Console.ReadLine());
+            String result = Convert.ToString(Convert.ToInt32(number, fromBase), toBase);
+            Console.WriteLine($"il numero convertito è {result}");
+            Console.ReadLine();
+        }
+    }
+}
